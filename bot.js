a30d451f2a4511f5d1ad026feff1b9b98b4886cc
haniking0 <43303520+haniking0@users.noreply.github.com>
2018-09-16 17:30:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,39 @@
-.
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = 'B'
+
+
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`BOSS_BOT|Bhelp`,"http://twitch.tv/S-F")
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start Time] ${new Date()}`);
+  console.log('ARE YOU READY?')
+  console.log('LETS GO BOT ONLINE!!')
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+});
+
+
+
+
+
+client.on("message", message => {
+      if (message.content === "Bping") {
+      const embed = new Discord.RichEmbed()
+  .setColor("RANDOM")
+  .addField('**Pong:**' , `${Date.now() - message.createdTimestamp}` + ' ms')
+  message.channel.sendEmbed(embed);
+    }
+});
+
+
+
+
+
+
+
+
+
+client.login(process.env.BOT_TOKEN);
